ec03c43ae6873f6626489d982f6a2385cc33759b
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-03-23 17:30:21 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+#1
+kopaytma = lambda x: x * 2
+
+res = kopaytma(5)
+print(res)
+
+
+#2
+kvadrat = lambda x: x ** 2
+
+res = kvadrat(4)
+print(res)
+
+
+#3
+kub = lambda x: x ** 3
+
+res = kub(4)
+print(res)
+
+
+#4
+summa = lambda a, b: a + b
+
+res = summa(8, 7)
+print(res)
+
+
+#5
+ayirish = lambda a, b: a - b
+
+res = ayirish(8, 7)
+print(res)
+
+
+#6
+kopaytma = lambda a, b: a * b
+
+res = kopaytma(4, 3)
+print(res)
+
+
+#7
+boluvchi = lambda a, b: a // b
+
+res = boluvchi(82, 2)
+print(res)
+
+
+#8
+qoshish = lambda x: x + 10
+
+res = qoshish(14)
+print(res)
+
+
+#9
+ayirish = lambda x: x - 5
+
+res = ayirish(45)
+print(res)
+
+
+#10
+bolish = lambda x: x // 3
+
+res = bolish(12)
+print(res)
